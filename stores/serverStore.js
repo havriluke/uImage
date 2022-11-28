5152ff3828b1791a8aa72a98e8178f3e8e526ae6
havriluke <havrylyuk.post@gmail.com>
2022-11-28 20:57:51 +0200

    s2

--- stores/serverStore.js
+++ stores/serverStore.js
@@ -13,19 +13,19 @@ class ServerStore {
     
 
     uploadImage(image, storage, url, redirectFunc) {
-        if (fs.existsSync(path.join(__dirname, `../static/0/${image}`))) {
+        if (fs.existsSync(`./static/0/${image}`)) {
             redirectFunc()
             return
         }
 
         while (this._storage + storage > this._storageLimit) {
             const deleteImage = this._imgs[0]
-            fs.unlinkSync(path.join(__dirname, `../static/0/${deleteImage.name}`))
+            fs.unlinkSync(`./static/0/${deleteImage.name}`)
             this._storage -= deleteImage.storage
             this._imgs = this._imgs.filter((img, i) => i !== 0)
         }
 
-        const file = fs.createWriteStream(path.join(__dirname, `../static/0/${image}`))
+        const file = fs.createWriteStream(`./static/0/${image}`)
         https.get(url, (response) => {
             response.pipe(file)
             file.on("finish", () => {
@@ -40,8 +40,8 @@ class ServerStore {
     }
 
     deleteImage(image) {
-        if (fs.existsSync(path.join(__dirname, `../static/0/${image}`))) {
-            fs.unlinkSync(path.join(__dirname, `../static/0/${image}`))
+        if (fs.existsSync(`./static/0/${image}`)) {
+            fs.unlinkSync(`./static/0/${image}`)
             this._storage -= this._imgs.filter((img) => img.name === image)[0].storage
             this._imgs = this._imgs.filter((img) => img.name !== image)
         }
